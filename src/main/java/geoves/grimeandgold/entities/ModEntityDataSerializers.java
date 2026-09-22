925e992d17009fad84076adee5f826c39382d3c8
Meniscus69000 <coco.nut6379@gmail.com>
2026-09-23 00:10:44 +1000

    Sift grub states (fixed something)

--- src/main/java/geoves/grimeandgold/entities/ModEntityDataSerializers.java
+++ src/main/java/geoves/grimeandgold/entities/ModEntityDataSerializers.java
@@ -2,14 +2,14 @@ package geoves.grimeandgold.entities;
 
 import geoves.grimeandgold.GrimeAndGold;
 import geoves.grimeandgold.entities.custom.siftgrub.SiftGrub;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityDataRegistry;
 import net.minecraft.network.syncher.EntityDataSerializer;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.world.entity.animal.sniffer.Sniffer;
+import net.minecraft.resources.Identifier;
 
 public class ModEntityDataSerializers {
     public static final EntityDataSerializer<SiftGrub.State> SIFT_GRUB_STATE = EntityDataSerializer.forValueType(SiftGrub.State.STREAM_CODEC);
 
     public static void register() {
-        EntityDataSerializers.registerSerializer(SIFT_GRUB_STATE);
+        FabricEntityDataRegistry.register(Identifier.fromNamespaceAndPath(GrimeAndGold.MOD_ID, "sift_grub_state"), SIFT_GRUB_STATE);
     }
 }
